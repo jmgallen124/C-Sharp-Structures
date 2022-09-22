72e9026579cd59d4ddece5aa329071491948c9d1
jmgallen <35116410+jmgallen@users.noreply.github.com>
2022-09-22 17:04:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Jeremy Gallen's sample program to demonstrate structures
+
+namespace Structures
+{
+    // Define character structure
+    public struct Character
+    {
+        public string Name;
+        public string Vocation;
+        public char Gender;
+        public int Age;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare character
+            Character Hero;
+
+            // Hero data
+            Hero.Name = "Jeremy";
+            Hero.Vocation = "Druid";
+            Hero.Gender = 'M';
+            Hero.Age = 38;
+
+            // Output Hero data
+            Console.WriteLine("Name: " + Hero.Name);
+            Console.WriteLine("Vocation: " + Hero.Vocation);
+            Console.WriteLine("Gender: " + Hero.Gender);
+            Console.WriteLine("Age: " + Hero.Age);
+        }
+    }
+}
